a0b07d4429ac924b1a4ca0d97eb7daaccc12d6dc
Priyanshi Chauhan <priyanshi.chauhan@opstree.com>
2024-09-09 17:58:06 +0530

    update

--- build.sh
+++ build.sh
@@ -12,12 +12,13 @@ GIT_URL=$(getDeploymentGitUrl)
 GIT_BRANCH=$(getDeploymentGitBranch)
 GIT_COMMIT_MSG=$(getGitCommitMsg)
 GIT_COMMIT_SHA=$(getGitCommitSha)                                                                                                                                                                                                                                                                                                                                                               
-DESCRIPTION="Deploy Details :- gitUrl: $GIT_URL, gitBranch: $GIT_BRANCH, gitCommitSha: $GIT_COMMIT_SHA, gitCommitMsg: $GIT_COMMIT_MSG, dockerImage: ${VERSION}"
 USER=$(getDeploymentUser)
+
+DESCRIPTION="Deploy Details :- gitUrl: $GIT_URL, gitBranch: $GIT_BRANCH, gitCommitSha: $GIT_COMMIT_SHA, gitCommitMsg: $GIT_COMMIT_MSG, dockerImage: ${VERSION}"
 
 sleep $SLEEP_DURATION
 
-logInfoMessage "I'll create a Deployment tracking for entity GUID: ${ENTITY_GUID}"
+logInfoMessage "I will update deployment change tracking"
 
 newrelic profile add --profile "${NEW_RELIC_PROFILE}" --region "${NEW_RELIC_REGION}" --apiKey "${NEW_RELIC_API_KEY}" --accountId "${NEW_RELIC_ACCOUNT_ID}" --licenseKey "${NEW_RELIC_LICENSE_KEY}" > /dev/null 2>&1
 
@@ -28,28 +29,29 @@ else
     logInfoMessage "Successfully authenticated with New Relic CLI."
 fi
 
-# Search for the New Relic entity based on the provided APM name
 if ! ENTITY_INFO=$(newrelic entity search --name "$APM_NAME"); then
-    logErrorMessage "Failed to retrieve entity information from New Relic. Please check your authentication and APM name."
+    logErrorMessage "Failed to retrieve entity information from New Relic. Please check your authentication and Entity NAME."
     exit 1
 else
     logInfoMessage "Successfully retrieved entity information from New Relic."
 fi
 
-# Extract the ENTITY_GUID from the retrieved entity information using jq
 if ! ENTITY_GUID=$(echo "$ENTITY_INFO" | jq -r .guid); then
-    logErrorMessage "Failed to extract ENTITY_GUID from New Relic entity information. Please ensure the APM name is correct and valid data is returned."
+    logErrorMessage "Failed to extract ENTITY_GUID from New Relic entity information. Please ensure the Entity NAME is correct and valid data is returned."
     exit 1
 else
     logInfoMessage "Successfully extracted ENTITY_GUID: $ENTITY_GUID from the entity information."
 fi
 
+logInfoMessage "Entity NAME: ${APM_NAME}"
+logInfoMessage "Entity GUID: ${ENTITY_GUID}"
+
 if newrelic entity deployment create --guid "${ENTITY_GUID}" --version "${VERSION}" \
-  --description "${DESCRIPTION}" --user "${USER}" --deploymentType "${DEPLOYMENT_TYPE}" 
-then
-  logInfoMessage "Deployment tracking successful for entity GUID: ${ENTITY_GUID}."
-  generateOutput $ACTIVITY_SUB_TASK_CODE true "Deployment tracking successful for entity GUID: ${ENTITY_GUID}."
+  --description "${DESCRIPTION}" --user "${USER}" --deploymentType "${DEPLOYMENT_TYPE}"; then
+    logInfoMessage "Deployment tracking successful for Entity NAME: ${APM_NAME}, Entity GUID: ${ENTITY_GUID}."
+    generateOutput $ACTIVITY_SUB_TASK_CODE true "Deployment tracking successful for Entity NAME: ${APM_NAME}, Entity GUID: ${ENTITY_GUID}."
 else
-  logErrorMessage "Deployment tracking failed for entity GUID: ${ENTITY_GUID}."
-  generateOutput $ACTIVITY_SUB_TASK_CODE false "Deployment tracking failed for entity GUID: ${ENTITY_GUID}."
+    logErrorMessage "Deployment tracking failed for Entity NAME: ${APM_NAME}, Entity GUID: ${ENTITY_GUID}."
+    generateOutput $ACTIVITY_SUB_TASK_CODE false "Deployment tracking failed for Entity NAME: ${APM_NAME}, Entity GUID: ${ENTITY_GUID}."
 fi
+
